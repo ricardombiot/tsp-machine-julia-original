8669b053bce7372b79be329021780558cc35c66e
Ricardo M. Biot <>
2021-04-21 19:26:36 +0200

    Hamiltonian12Test parallel

--- test_build_grafs_12/run_test.sh
+++ test_build_grafs_12/run_test.sh
@@ -1,1 +1,1 @@
-julia --project=./../ --threads 1 ./../test_build_grafs_12/test_hamiltonian.jl $1
+julia --project=./../ --threads 2 ./../test_build_grafs_12/test_hamiltonian.jl $1
--- test_build_grafs_12/test_hamiltonian.jl
+++ test_build_grafs_12/test_hamiltonian.jl
@@ -37,11 +37,13 @@ function test_clasification_hal_machine(n :: Color, id :: Int64, path_results ::
     is_hamiltonian = SolutionGraphReader.have_solution(machine)
     write_result_is_hamiltonian!(path_results, id, is_hamiltonian)
 
+    #=
     if is_hamiltonian
-        println("TSPMachine: El grafo $id, hamiltoniano [TRUE]")
+        println("HalMachine: El grafo $id, hamiltoniano [TRUE]")
     else
-        println("TSPMachine: El grafo $id, hamiltoniano [FALSE]")
+        println("HalMachine: El grafo $id, hamiltoniano [FALSE]")
     end
+    =#
 end
 
 function test_clasification_tsp_machine(n :: Color, id :: Int64, path_results :: String)
@@ -56,33 +58,36 @@ function test_clasification_tsp_machine(n :: Color, id :: Int64, path_results ::
     is_hamiltonian = SolutionGraphReader.have_solution(machine)
     write_result_is_hamiltonian!(path_results, id, is_hamiltonian)
 
+    #=
     if is_hamiltonian
-        println("HalMachine: El grafo $id, hamiltoniano [TRUE]")
+        println("TSPMachine: El grafo $id, hamiltoniano [TRUE]")
     else
-        println("HalMachine: El grafo $id, hamiltoniano [FALSE]")
+        println("TSPMachine: El grafo $id, hamiltoniano [FALSE]")
     end
+    =#
 end
+
+
+function test_clasification_tsp_brute_force(n :: Color, id :: Int64, path_results :: String)
+    path = "./../../../../test_build_grafs_12/data/grafs$n/tsp"
+    graf = GrafGenerator.read_tsplib_file("$id",path,".tsp")
+
+    color_origin = Color(0)
+    b_km = Km(n)
+    machine = TSPBruteForce.new(graf, b_km, color_origin)
+    TSPBruteForce.execute!(machine)
+
+    is_hamiltonian = TSPBruteForce.have_solution(machine)
+    write_result_is_hamiltonian!(path_results, id, is_hamiltonian)
 
     #=
-function read_directory(directory :: String, extenstion :: String, graf_start :: Int64 = -1, graf_end :: Int64 = -1)
-    cd(directory)
-    if graf_start == -1
-        lista_grafs = readdir()
+    if is_hamiltonian
+        println("BruteForceMachine: El grafo $id, hamiltoniano [TRUE]")
     else
-        lista_grafs = Array{String, 1}()
-        for name in graf_start:graf_end
-            path = "./$(name)$(extenstion)"
-            if !isfile(path)
-                break
-            end
-
-            push!(lista_grafs, path)
-        end
+        println("BruteForceMachine: El grafo $id, hamiltoniano [FALSE]")
     end
-
-    return lista_grafs
+    =#
 end
-=#
 
 function main(args)
      n = parse(UInt128,first(args))
@@ -90,36 +95,48 @@ function main(args)
      base_path = "./data/results$n"
      path_tsp_machine = prepare_folders_results(base_path, "tsp_machine")
      path_hal_machine = prepare_folders_results(base_path, "hal_machine")
+     path_tsp_brute = prepare_folders_results(base_path, "tsp_brute")
 
      println("### Classication ###")
 
      total = Int64(2^((n*(n-1))/2))
-     for id in 0:total-1
+     Threads.@threads for id in 0:total-1
          test_clasification_tsp_machine(n, id, path_tsp_machine)
          test_clasification_hal_machine(n, id, path_hal_machine)
+         test_clasification_tsp_brute_force(n, id, path_tsp_brute)
      end
 
      println("### Verification ###")
 
      total_ok = 0
+     total_tsp_verficate = 0
      for id in 0:total-1
          result_tsp = read_result(path_tsp_machine, id)
          result_hal = read_result(path_hal_machine, id)
 
+         result_brute = read_result(path_tsp_brute, id)
+         verificate_tsp = result_tsp == result_brute
+
          if result_tsp == result_hal && result_tsp != "ERROR"
              total_ok += 1
+
+             if verificate_tsp
+                 total_tsp_verficate += 1
+             end
          else
              println("Error $id")
          end
      end
 
      rate = (total_ok / total) * 100
+     rate_tsp_verificate = (total_tsp_verficate/ total) * 100
 
      println("## Summary Test##")
      println("Graf. N: $n")
      println("TOTAL: $total")
      println("TOTAL OK: $total_ok")
      println("RATE: $rate%")
+     println("RATE TSP VERIFICATE: $rate_tsp_verificate%")
 
 
 end
